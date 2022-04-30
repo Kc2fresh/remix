25660dbe546c8d139dc6861d4ecceeb9e1829fd7
KELECHI <Kelechixyz@yahoo.com>
2022-04-30 15:06:27 +0800

    MAIN

---
+++ count/backend/django_app/mainapp/local_settings.py
@@ -0,0 +1,13 @@
+ 
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'countdb',
+        'USER': 'postgres',
+        'PASSWORD': 'emilesande',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
