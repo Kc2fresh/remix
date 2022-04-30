7d1115931b99c3b8489e2c4463a1c0f40bf6625b
KELECHI <Kelechixyz@yahoo.com>
2022-04-30 15:34:01 +0800

    latest

--- count/backend/django_app/counterinfer/apps.py
+++ count/backend/django_app/counterinfer/apps.py
@@ -1,6 +1,19 @@
 from django.apps import AppConfig
+import torch
+from transformers import RobertaTokenizer, RobertaForSequenceClassification
+#import joblib
+import os
+
 
 
 class CounterinferConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'counterinfer'
+    name='counterinfer'
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    MLMODEL_FOLDER = os.path.join(BASE_DIR, 'counterinfer/mlmodel/')
+    MLMODEL_FILE = os.path.join(MLMODEL_FOLDER, "predict.pth" )
+    model = RobertaForSequenceClassification.from_pretrained("roberta-base",num_labels=2)
+    tokenizer = RobertaTokenizer.from_pretrained('roberta-base', do_lower_case=True)
+    device = torch.device('cpu')
+    model.load_state_dict(torch.load(MLMODEL_FILE,map_location=torch.device('cpu')))
+    model.eval()
+   
--- count/backend/django_app/mainapp/settings.py
+++ count/backend/django_app/mainapp/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'counterinfer'
+    'corsheaders',
+    'rest_framework',
+    'counterinfer',
 ]
 
 MIDDLEWARE = [
